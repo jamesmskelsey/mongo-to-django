fcd1009820ea4c2a81607bcb840306bffc4b3764
James Kelsey <jamesmskelsey@gmail.com>
2022-04-24 11:39:27 -0700

    refactor

---
+++ model_reader.py
@@ -0,0 +1,67 @@
+from typing import Dict
+from pymongo import MongoClient
+
+def read(client: MongoClient, database: str, collection: str, limit:int=1000, filter:dict={}) -> Dict: 
+  """
+  Given a pymongo client, a database name, and a collection name, finds the field names
+  and the types of data in each field and returns them as a dictionary.
+
+  Optional:
+  limit: Number of docs to analyze at a time.
+  filter: A DSL query to limit the docs to a subset. 
+  """
+  number_of_documents = 0
+  skip=0
+  counts = {
+    # Example of end result this dict should be.
+    # 'field_name': {
+    #   'type_1': 10,
+    #   'type_2': 5
+    # },
+    # 'next_field_name': {
+    #   'type_1': 5,
+    #   'type_2': 10,
+    # }
+  }
+
+  aggregation_count_of_documents_in_collection = client[database][collection].aggregate([
+    {
+        '$count': 'count'
+    }
+  ])
+
+  for count in aggregation_count_of_documents_in_collection:
+    # Cursors are weird.
+    number_of_documents = count['count']
+    print(f"Found {number_of_documents} documents in collection: '{database}'.")
+
+  while skip < number_of_documents:
+    # We'll eventually loop across the number_of_documents, paginating out 1,000 at a time and recording the results.
+    results = client[database][collection].find(
+      filter=filter,
+      skip=skip,
+      limit=limit
+    )
+
+    for result in results:
+      for field_name, field_type in result.items():
+        # I don't care what the value is - only what the type is, so convert is straight away.
+        field_type = type(field_type)
+        # option 1 - field name does not exist.
+        ## check for the field name - it doesn't exist, so we create one.
+        if field_name not in counts:
+          counts[field_name] = {
+            field_type: 1
+          }
+        # option 2 - field name does exist, the type does not exist.
+        elif field_name in counts and field_type not in counts[field_name]:
+          counts[field_name][field_type] = 1
+        # option 3 - field name does exist, type does exist.
+        elif field_name in counts and field_type in counts[field_name]:
+          counts[field_name][field_type] += 1
+    skip += 1000
+    
+  return counts
+  # Uncomment to print to console.
+  # for k, v in counts.items():
+  #   print(k, v)
--- mtd.py
+++ mtd.py
@@ -1,13 +1,18 @@
-from itertools import count
 import os
 from dotenv import load_dotenv
 import pymongo
+import model_reader
 
 load_dotenv()
 
 CONNECTION_STRING = os.getenv('CONNECTION_STRING')
 DATABASE = os.getenv('DATABASE_NAME')
 COLLECTION = os.getenv('COLLECTION_TO_ANALYZE')
+
+if DATABASE == '':
+  raise ValueError("Database name cannot be a blank string. Edit the .env file.")
+if COLLECTION == '':
+  raise ValueError("The collection name must be provided in .env")
 
 print(f"Connecting to: {CONNECTION_STRING}")
 client = pymongo.MongoClient(CONNECTION_STRING, serverSelectionTimeoutMS=5000)
@@ -19,62 +24,12 @@ try:
     connected = True
   
 except Exception:
-  print("Unable to connect to the server.")
+  print("Unable to connect to the server. Cancelling.")
 
 if connected:
-  print("Connected to server. Now attempting to access documents.")
-  number_of_documents = 0
-  filter = {}
-  skip=0
-  limit=1000
-  counts = {
-    # Example of end result this dict should be.
-    # 'field_name': {
-    #   'type_1': 10,
-    #   'type_2': 5
-    # },
-    # 'next_field_name': {
-    #   'type_1': 5,
-    #   'type_2': 10,
-    # }
-  }
+  print("Connected to server. Now attempting to access documents.\n")
+  
+  results = model_reader.read(client, DATABASE, COLLECTION)
 
-  aggregation_count_of_documents_in_collection = client['g2link']['companies'].aggregate([
-    {
-        '$count': 'count'
-    }
-  ])
-
-  for count in aggregation_count_of_documents_in_collection:
-    # Cursors are weird.
-    number_of_documents = count['count']
-    print(f"Found {number_of_documents} documents in collection: '{COLLECTION}'.")
-
-  while skip < number_of_documents:
-    # We'll eventually loop across the number_of_documents, paginating out 1,000 at a time and recording the results.
-    results = client[DATABASE][COLLECTION].find(
-      filter=filter,
-      skip=skip,
-      limit=limit
-    )
-
-    for result in results:
-      for field_name, field_type in result.items():
-        # I don't care what the value is - only what the type is, so convert is straight away.
-        field_type = type(field_type)
-        # option 1 - field name does not exist.
-        ## check for the field name - it doesn't exist, so we create one.
-        if field_name not in counts:
-          counts[field_name] = {
-            field_type: 1
-          }
-        # option 2 - field name does exist, the type does not exist.
-        elif field_name in counts and field_type not in counts[field_name]:
-          counts[field_name][field_type] = 1
-        # option 3 - field name does exist, type does exist.
-        elif field_name in counts and field_type in counts[field_name]:
-          counts[field_name][field_type] += 1
-    skip += 1000
-
-  for k, v in counts.items():
+  for k, v in results.items():
     print(k, v)
